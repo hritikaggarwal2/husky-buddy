3e176b4d59d7a71e5a1c4d9caca093e456faa1de
hritikaggarwal2 <hritikaggarwal2@gmail.com>
2021-04-22 14:57:31 -0700

    restructured code into folders + added firebase + added routes + added sass

---
+++ web/src/screens/Dashboard.js
@@ -0,0 +1,21 @@
+// Add Imports
+
+export default function Dashboard() {
+  return (
+    <div className="Dashboard">
+      <header className="Dashboard-header">
+        <p>
+          Edit <code>Dashboard.js</code> and save to reload.
+        </p>
+        <a
+          className="Dashboard-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
