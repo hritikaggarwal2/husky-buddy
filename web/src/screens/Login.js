809c3c93a90193cf823fed05efbcc32dae03494c
fourkhan <thornegarvinspam@gmail.com>
2021-04-28 11:55:12 -0700

    add eslint warning suppression

--- web/src/screens/Login.js
+++ web/src/screens/Login.js
@@ -29,6 +29,7 @@ export default function Login() {
     } else {
 
       // courtesy email validation
+      // eslint-disable-next-line
       const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
       var matches = email.match(emailRegex);
       //console.log(matches);
