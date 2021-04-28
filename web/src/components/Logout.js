79c6dd02e8dbef70973c1048bc5fa11b867a53e0
fourkhan <thornegarvinspam@gmail.com>
2021-04-28 13:03:09 -0700

    add login functionality

--- web/src/components/Logout.js
+++ web/src/components/Logout.js
@@ -4,35 +4,38 @@ import firebase from "firebase/app";
 import { useUser } from "../providers/UserProvider";
 
 export default function Logout() {
-    const user = useUser.user;
+  const user = useUser.user;
 
-    function enabled() {
-      return (user !== null);
+  function enabled() {
+    return user !== null;
+  }
+
+  function handleSubmit(_event) {
+    if (!enabled()) {
+      return;
     }
 
-    function handleSubmit(_event) {
-      if (!enabled()) {
-        return;
-      }
-
-      console.log("signing user out");
-      firebase.auth().signOut().then((_) => {
+    console.log("signing user out");
+    firebase
+      .auth()
+      .signOut()
+      .then((_) => {
         console.log("signout succeeded!");
-      }).catch((error) => {
+      })
+      .catch((error) => {
         // XXX tgarvin: maybe surface this to the user somehow? not sure what errors this can throw
         // TODO: resolve this in a better way
         console.log("Signout failure: " + error.code + ": " + error.message);
       });
-    }
+  }
 
-    return (
-        <div classname="logout-button">
-            <form className="logout-wrapper" onSubmit={handleSubmit}>
-                <button type="submit" disabled={!enabled()}>
-                Logout
-                </button>
-            </form>
-        </div>
-    );
+  return (
+    <div classname="logout-button">
+      <form className="logout-wrapper" onSubmit={handleSubmit}>
+        <button type="submit" disabled={!enabled()}>
+          Logout
+        </button>
+      </form>
+    </div>
+  );
 }
-
