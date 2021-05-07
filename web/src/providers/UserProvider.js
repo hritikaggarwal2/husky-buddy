3ba9904d392753ada6187775c7f0e76d4a4a4f76
fourkhan <thornegarvinspam@gmail.com>
2021-05-06 21:47:51 -0700

    initial work

--- web/src/providers/UserProvider.js
+++ web/src/providers/UserProvider.js
@@ -1,20 +1,44 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import firebase from "firebase/app";
 import "firebase/auth";
+import { UserClassConverter } from "../data/UserClass";
 
-export const UserContext = createContext();
+const UserContext = createContext();
 
 export function useUser() {
-  return useContext(UserContext);
+  const context = React.useContext(UserContext);
+  if (!context) {
+    throw new Error(`useUser must be used within a UserProvider`);
+  }
+  return context;
+}
+
+async function getHuskyUserFromReactUser(reactUser) {
+  console.log("react user uid:" + reactUser.uid);
+
+  let huskyUserRef = firebase
+    .firestore()
+    .collection("Users")
+    .doc(reactUser.uid);
+
+  const huskyUser = await huskyUserRef.get();
+  return huskyUser.data();
 }
 
 export default function UserProvider(props) {
   const [user, setUser] = useState(firebase.auth().currentUser);
-  const [load, setLoad] = useState(false);
+  const [huskyUser, setHuskyUser] = React.useState();
+  const [load, setLoad] = React.useState(false);
 
   useEffect(() => {
     const unsubscribe = firebase.auth().onAuthStateChanged((userAuth) => {
       setUser(userAuth);
+      getHuskyUserFromReactUser(userAuth).then((huskyUser) => {
+        setHuskyUser(huskyUser);
+        console.log("husky user:");
+        console.log(huskyUser);
+      });
+
       setLoad(true);
     });
 
@@ -25,7 +49,7 @@ export default function UserProvider(props) {
   }, []);
 
   return (
-    <UserContext.Provider value={{ user, load }}>
+    <UserContext.Provider value={{ user, load, huskyUser }}>
       {props.children}
     </UserContext.Provider>
   );
