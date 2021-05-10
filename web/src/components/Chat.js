64b3dc12329fba681dc91088ef5e520ee78e1d85
ilya <ilyakuks@uw.edu>
2021-05-10 15:29:26 -0400

    Fixing suggestions

--- web/src/components/Chat.js
+++ web/src/components/Chat.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import CreateGroup from "../components/CreateGroup";
 import "../styles/CreateGroup.css";
 import "../components/MyGroupPanel";
@@ -15,72 +15,79 @@ import "../styles/Chat.css";
  *                  the chat belongs.
  */
 export default function Chat(props) {
-    // TODO: Update group ID to the actual one
-    const [groupID, setGroupID] = useState("2jQWkuzyGzmzSQWpGOfC");
-    const [messages, setMessages] = useState([]);
-    const [outMessage, setOutMessage] = useState("");
-    //const user = useUser.user;
-    const user = "4zmEw8xE1ehszvmSV7Vz";
+  // TODO: Update group ID to the actual one
+  const [groupID, setGroupID] = useState("2jQWkuzyGzmzSQWpGOfC");
+  const [messages, setMessages] = useState([]);
+  const [outMessage, setOutMessage] = useState("");
+  //const user = useUser.user;
+  const user = "4zmEw8xE1ehszvmSV7Vz";
 
-    useEffect(() =>{
-        // Store all chat messages and update if there
-        // is a new one coming in
-        const chatRef = firebase.firestore().collection("Chats").doc(groupID)
-                                .collection("chat").orderBy('time')
-                                .onSnapshot((snapshot) => {
-                const tempMessages = [];
-                snapshot.forEach( (message) => {
-                    tempMessages.push(message.data())
-                });
-                setMessages(tempMessages)});
-        }, []);
+  useEffect(() => {
+    // Store all chat messages and update if there
+    // is a new one coming in
+    const chatRef = firebase
+      .firestore()
+      .collection("Chats")
+      .doc(groupID)
+      .collection("chat")
+      .orderBy("time")
+      .onSnapshot((snapshot) => {
+        const tempMessages = [];
+        snapshot.forEach((message) => {
+          tempMessages.push(message.data());
+        });
+        setMessages(tempMessages);
+      });
+  }, []);
 
-
-    // Sends message to Firebase Database
-    function sendMessage() {
-        // SEND MESSAGE TO FIREBASE SERVER
-        const chatRef = firebase.firestore().collection("Chats").doc(groupID)
-            .collection("chat");
-        let result = chatRef.add({
-            content: outMessage,
-            owner: user,
-            time: firebase.firestore.Timestamp.now()
-        }).then((docRef) => {
-            /*
+  // Sends message to Firebase Database
+  function sendMessage() {
+    const chatRef = firebase
+      .firestore()
+      .collection("Chats")
+      .doc(groupID)
+      .collection("chat");
+    let result = chatRef
+      .add({
+        content: outMessage,
+        owner: user,
+        time: firebase.firestore.Timestamp.now(),
+      })
+      .then((docRef) => {
+        /*
             // TODO: change "4zmEw8xE1ehszvmSV7Vz" to actual userID
             refUsers.doc("4zmEw8xE1ehszvmSV7Vz").update({
                 groups: arrayUnion(docRef.id)
             });
              */
-            alert("Chat Sent!");
-        });
-        setOutMessage("");
-    }
+        alert("Chat Sent!");
+      });
+    setOutMessage("");
+  }
 
-    // Displays a single message to the chat window
-    // ADD MORE FUNCTIONALITY, I.e. display who sent it and time
-    function displayMessage(message) {
-        return(
-            <div className="MessageContents">
-                <p> Message: {message.content} </p>
-                <p> Sent by: {message.owner} </p>
-                <p> Time sent: {message.time.toString()} </p>
-                <p> ... </p>
-            </div>
-        )
-    }
-
+  // Displays a single message to the chat window
+  // ADD MORE FUNCTIONALITY, I.e. display who sent it and time
+  function displayMessage(message) {
     return (
-        <div className="ChatWindow">
-            <div className="Chats">
-                {messages.map(displayMessage)}
-            </div>
-            <input className="MessageInputer"
-                   value={outMessage}
-                   onChange={(event) => setOutMessage(event.target.value)}
-                   type={'string'}
-            />
-            <button onClick={sendMessage}> Send Message</button>
-        </div>
+      <div className="MessageContents">
+        <p> Message: {message.content} </p>
+        <p> Sent by: {message.owner} </p>
+        <p> Time sent: {message.time.toString()} </p>
+        <p> ... </p>
+      </div>
     );
+  }
+
+  return (
+    <div className="ChatWindow">
+      <div className="Chats">{messages.map(displayMessage)}</div>
+      <input
+        className="MessageInputer"
+        value={outMessage}
+        onChange={(event) => setOutMessage(event.target.value)}
+        type={"string"}
+      />
+      <button onClick={sendMessage}> Send Message</button>
+    </div>
+  );
 }
--- web/src/components/PopUpForm.js
+++ web/src/components/PopUpForm.js
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import ValueInputer from "./ValueInputer";
-import '../styles/CreateGroup.css';
+import "../styles/CreateGroup.css";
 import { useUser } from "../providers/UserProvider";
 
 /**
@@ -14,75 +14,116 @@ import { useUser } from "../providers/UserProvider";
  *  action: value to display on the submit button
  */
 export default function PopUpForm(props) {
-    const [maxGroupSize, setMaxGroupSize] = useState(0);
-    const [groupName, setGroupName] = useState("");
-    const [classPrefix, setClassPrefix] = useState("");
-    const [classNum, setClassNum] = useState(0);
-    const [classSection, setClassSection] = useState("");
-    const [topics, setTopics] = useState(0);
-    const [meetInPerson, setMeetInPerson] = useState(false);
-    const user = useUser.user;
+  const [maxGroupSize, setMaxGroupSize] = useState(0);
+  const [groupName, setGroupName] = useState("");
+  const [classPrefix, setClassPrefix] = useState("");
+  const [classNum, setClassNum] = useState(0);
+  const [classSection, setClassSection] = useState("");
+  const [topics, setTopics] = useState(0);
+  const [meetInPerson, setMeetInPerson] = useState(false);
+  const user = useUser.user;
 
-    function closeForm() {
-        props.onChange("", "", "", "", "", "");
+  function closeForm() {
+    props.onChange("", "", "", "", "", "");
+  }
+
+  function recordData() {
+    // ***** VALIDATE INPUT *****
+    if (maxGroupSize <= 1) {
+      alert("Enter a valid group size (must be greater than 1).");
+      return;
+    }
+    if (groupName === "" || groupName == null) {
+      alert("Enter a valid group name.");
+      return;
     }
 
-    function recordData() {
-        // ***** VALIDATE INPUT *****
-        if (maxGroupSize <= 1) {
-            alert("Enter a valid group size (must be greater than 1).");
-            return;
-        }
-        if (groupName === "" || groupName === null) {
-            alert("Enter a valid group name.");
-            return;
-        }
-
-        if (classPrefix === "" || groupName === null) {
-            alert("Enter a valid class prefix (Example: CSE, ENGL, MATH, etc).");
-            return;
-        }
-
-        if (classNum <= 0 || classNum > 1000) {
-            alert("Enter a valid class number.");
-            return;
-        }
-
-
-        //@TODO // Parse topics string into an array of topics and pass that array to props.onChange()
-
-        props.onChange(maxGroupSize, groupName, classPrefix, classNum, classSection, topics, meetInPerson, user);
+    if (classPrefix === "" || groupName == null) {
+      alert("Enter a valid class prefix (Example: CSE, ENGL, MATH, etc).");
+      return;
     }
 
-    function recordTopics(topicsIn) {
-        let topicsWanted = [];
-        if (topicsIn !== null) {
-            topicsWanted = topicsIn.split(" ");
-        }
-
-        console.log(topicsWanted);
-        setTopics(topicsWanted);
+    if (classNum <= 0 || classNum > 1000) {
+      alert("Enter a valid class number (between 0 and 1000)");
+      return;
     }
 
-    return (
-        <div className="form">
-            <div className="form_content">
-                <span className="formCloseButton" onClick={closeForm}>&times;</span>
-                <ValueInputer title={'Group Name'} type={'string'} onChange={input => setGroupName(input)}/>
-                <ValueInputer title={'Class Prefix'} type={'string'} onChange={input => setClassPrefix(input)}/>
-                <ValueInputer title={'Class Number'} type={'number'} onChange={input => setClassNum(input)}/>
-                <ValueInputer title={'Class Section (Optional)'} type={'string'}
-                              onChange={input => setClassSection(input)}/>
-                <ValueInputer title={'Topics of Interest (Optional)'} type={'string'} onChange={recordTopics}/>
-                <ValueInputer title={'Max Group Size'} min={0} max={props.maxGroupSize} type={'number'}
-                              onChange={input => setMaxGroupSize(input)}/>
+    //@TODO // Parse topics string into an array of topics and pass that array to props.onChange()
 
-                <label>Meet In Person
-                    <input className="checkbox" type="checkbox" checked={meetInPerson}
-                           onClick={() => setMeetInPerson(prevVal => !prevVal)}/>
-                </label>
-                <button className="formButton" onClick={recordData}>{props.action}</button>
-            </div>
-        </div>
+    props.onChange(
+      maxGroupSize,
+      groupName,
+      classPrefix,
+      classNum,
+      classSection,
+      topics,
+      meetInPerson,
+      user
     );
+  }
+
+  function recordTopics(topicsIn) {
+    let topicsWanted = [];
+    if (topicsIn !== null) {
+      topicsWanted = topicsIn.split(" ");
+    }
+
+    console.log(topicsWanted);
+    setTopics(topicsWanted);
+  }
+
+  return (
+    <div className="form">
+      <div className="form_content">
+        <span className="formCloseButton" onClick={closeForm}>
+          &times;
+        </span>
+        <ValueInputer
+          title={"Group Name"}
+          type={"string"}
+          onChange={(input) => setGroupName(input)}
+        />
+        <ValueInputer
+          title={"Class Prefix"}
+          type={"string"}
+          onChange={(input) => setClassPrefix(input)}
+        />
+        <ValueInputer
+          title={"Class Number"}
+          type={"number"}
+          onChange={(input) => setClassNum(input)}
+        />
+        <ValueInputer
+          title={"Class Section (Optional)"}
+          type={"string"}
+          onChange={(input) => setClassSection(input)}
+        />
+        <ValueInputer
+          title={"Topics of Interest (Optional)"}
+          type={"string"}
+          onChange={recordTopics}
+        />
+        <ValueInputer
+          title={"Max Group Size"}
+          min={0}
+          max={props.maxGroupSize}
+          type={"number"}
+          onChange={(input) => setMaxGroupSize(input)}
+        />
+
+        <label>
+          Meet In Person
+          <input
+            className="checkbox"
+            type="checkbox"
+            checked={meetInPerson}
+            onClick={() => setMeetInPerson((prevVal) => !prevVal)}
+          />
+        </label>
+        <button className="formButton" onClick={recordData}>
+          {props.action}
+        </button>
+      </div>
+    </div>
+  );
 }
