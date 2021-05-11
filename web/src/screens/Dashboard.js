e20569b34af4643af64c915c4faefb6e690095f0
fourkhan <thornegarvinspam@gmail.com>
2021-05-11 11:19:44 -0700

    make chat prettier and other improvemetns

--- web/src/screens/Dashboard.js
+++ web/src/screens/Dashboard.js
@@ -5,6 +5,8 @@ import "../styles/common.scss";
 import "../components/MyGroupPanel";
 import MyGroupPanel from "../components/MyGroupPanel";
 import { useUser } from "../providers/UserProvider";
+import LogOut from "../components/Logout";
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
   const [open, setOpen] = useState(false);
@@ -17,12 +19,18 @@ export default function Dashboard() {
 
       {/* MAIN DASHBOARD VIEW */}
       <div className="dashboard container d-flex col justify-center align-center">
-        <h1 className="title">HuskyBuddy.</h1>
+        <Link to="/dashboard">
+          <h1 className="title">HuskyBuddy</h1>
+        </Link>
         <h2>Welcome, {user.display_name}</h2>
         <MyGroupPanel groups={["Group1", "Group2"]} />
         <button className="btnPrimaryFill" onClick={() => setOpen(true)}>
           Create Group
         </button>
+      </div>
+      <div className="justify-center align-center">
+        Log out
+        <LogOut />
       </div>
     </>
   );
